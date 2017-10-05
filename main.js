c1ec25da939dc4c2c7abdf05d97b40e785934b4d
thurdas <31208075+thurdas@users.noreply.github.com>
2017-10-05 12:33:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 			var modal = document.getElementById("filesModal");
 			var modal2 = document.getElementById("videoModal");
+            var modal3 = document.getElementById("myModal3");
+            var modal4 = document.getElementById("myModal4");
 
 			var btn = document.getElementById("firstBtn");
 			var btn2 = document.getElementById("secondBtn");
+            var btn3 = document.getElementById("thirdBtn");
+            var btn4 = document.getElementById("fourthBtn");
 
 			var span = document.getElementsByClassName("close")[0];
 			var span2 = document.getElementsByClassName("close")[1];
+            var span3 = document.getElementsByClassName("close")[2];
+            var span4 = document.getElementsByClassName("close")[3];
 
 			btn.addEventListener("click", function() {
 			    modal.style.display = "block";
@@ -16,6 +22,19 @@
 			    modal2.style.display = "block";
 			    document.getElementById("modal-content2").className = "modal-content animated zoomIn";
 			});
+
+
+            btn3.addEventListener("click", function() {
+                modal3.style.display = "block";
+                document.getElementById("modal-content3").className = "modal-content animated zoomIn";
+            });
+
+            btn4.addEventListener("click", function() {
+                modal4.style.display = "block";
+                document.getElementById("modal-content4").className = "modal-content animated zoomIn";
+            });
+
+
 
 			span.onclick = function() {
 			    document.getElementById("modal-content").className = "modal-content animated zoomOut";
@@ -31,7 +50,22 @@
 			    }, 300);
 			};
 
-			window.onclick = function(event) {
+            span3.onclick = function() {
+                document.getElementById("modal-content3").className = "modal-content animated zoomOut";
+                window.setTimeout(function() {
+                    modal3.style.display = "none";
+                }, 300);
+            };
+
+            span4.onclick = function() {
+                document.getElementById("modal-content4").className = "modal-content animated zoomOut";
+                window.setTimeout(function() {
+                    modal4.style.display = "none";
+                }, 300);
+            };
+
+
+            window.onclick = function(event) {
 			    if (event.target === modal) {
 			        document.getElementById("modal-content").className = "modal-content animated zoomOut";
 			        window.setTimeout(function() {
@@ -40,7 +74,7 @@
 			    }
 			};
 
-			window2.onclick = function(event) {
+			window.onclick = function(event) {
 			    if (event.target === modal2) {
 			        document.getElementById("modal-content2").className = "modal-content animated zoomOut";
 			        window.setTimeout(function() {
@@ -48,3 +82,21 @@
 			        }, 300);
 			    }
 			};
+
+            window.onclick = function(event) {
+                if (event.target === modal3) {
+                    document.getElementById("modal-content3").className = "modal-content animated zoomOut";
+                    window.setTimeout(function() {
+                        modal3.style.display = "none";
+                    }, 300);
+                }
+            };
+
+            window.onclick = function(event) {
+                if (event.target === modal4) {
+                    document.getElementById("modal-content4").className = "modal-content animated zoomOut";
+                    window.setTimeout(function() {
+                        modal4.style.display = "none";
+                    }, 300);
+                }
+            };
